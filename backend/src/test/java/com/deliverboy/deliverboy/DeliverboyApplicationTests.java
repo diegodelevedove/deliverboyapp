2ee53675c9a428191cb6af7abc053d9abbed6898
Diego Delevedove de Oliveira <diego-delevedove.oliveira@serpro.gov.br>
2025-01-13 14:08:15 -0300

    Primeiro commit projeto em andamento

---
+++ backend/src/test/java/com/deliverboy/deliverboy/DeliverboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverboy.deliverboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
